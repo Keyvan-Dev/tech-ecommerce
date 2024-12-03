ce6b62989971a1f370083ef3cc3bf78b57a519ff
keyvan-dev <keyvanfordev@gmail.com>
2024-12-03 13:23:33 +0330

    Add a pagination button to product screen

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -2,10 +2,11 @@
 import { useEffect } from 'react';
 
 // Chakra UI
-import { Box, Wrap, WrapItem, Center } from '@chakra-ui/react';
+import { Box, Button, Center, Wrap, WrapItem } from '@chakra-ui/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
 
 // React Redux
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Components
 import ProductCard from '../components/ProductCard.jsx';
@@ -15,15 +16,19 @@ import { getProducts } from '../redux/actions/productActions.js';
 
 const ProductsScreen = () => {
 	const dispatch = useDispatch();
-	const { loading, error, products, pagination } = useSelector((state) => state.product);
+	const { loading, error, products, pagination, favoriteToggled } = useSelector((state) => state.product);
 
 	useEffect(() => {
 		dispatch(getProducts(1));
 	}, [dispatch]);
 
+	const paginationButtonClick = (page) => {
+		dispatch(getProducts(page));
+	};
+
 	return (
 		<>
-			{products.length > 1 && (
+			{products.length >= 1 && (
 				<Box>
 					<Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32' }}>
 						{products.map((product) => (
@@ -34,6 +39,27 @@ const ProductsScreen = () => {
 							</WrapItem>
 						))}
 					</Wrap>
+					{!favoriteToggled && (
+						<Wrap spacing='10px' justify='center' p='5'>
+							<Button colorScheme='cyan' onClick={() => paginationButtonClick(1)}>
+								<ArrowLeftIcon />
+							</Button>
+							{Array.from(Array(pagination.totalPags), (e, i) => {
+								return (
+									<Button
+										colorScheme={pagination.currentPage === i + 1 ? 'cyan' : 'gray'}
+										key={i}
+										onClick={() => paginationButtonClick(i + 1)}
+									>
+										{i + 1}
+									</Button>
+								);
+							})}
+							<Button colorScheme='cyan' onClick={() => paginationButtonClick(pagination.totalPags)}>
+								<ArrowRightIcon />
+							</Button>
+						</Wrap>
+					)}
 				</Box>
 			)}
 		</>
