a36eecae0959a8ec7da6d00b9a6f737c8db2f315
keyvan-dev <keyvanfordev@gmail.com>
2024-12-03 12:02:44 +0330

    Initialize React Router Dom in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 // Chakra UI
 import { ChakraProvider } from '@chakra-ui/react';
 
+// React Router Dom
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 // Screens
 import ProductsScreen from './screens/ProductsScreen.jsx';
 
+// Components
+import Header from './components/Header.jsx';
+
 const App = () => {
 	return (
-		<>
-			<ChakraProvider>
-				<ProductsScreen />
-			</ChakraProvider>
-		</>
+		<ChakraProvider>
+			<Router>
+				<Header />
+				<main>
+					<Routes path='/' element={<ProductsScreen />} />
+				</main>
+			</Router>
+		</ChakraProvider>
 	);
 };
 
