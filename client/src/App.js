1d854a547afb4257da42a26f05329c3925a55455
keyvan-dev <keyvanfordev@gmail.com>
2024-12-07 15:09:23 +0330

    Add new scrrens to react routers and fix some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 // Screens
 import ProductsScreen from './screens/ProductsScreen.jsx';
+import LandingScreen from './screens/LandingScreen.jsx';
+import ProductScreen from './screens/ProductScreen.jsx';
 
 // Components
 import Header from './components/Header.jsx';
@@ -16,7 +18,11 @@ const App = () => {
 			<Router>
 				<Header />
 				<main>
-					<Routes path='/' element={<ProductsScreen />} />
+					<Routes>
+						<Route path='/products' element={<ProductsScreen />} />
+						<Route path='/' element={<LandingScreen />} />
+						<Route path='/product/:id' element={<ProductScreen />} />
+					</Routes>
 				</main>
 			</Router>
 		</ChakraProvider>
