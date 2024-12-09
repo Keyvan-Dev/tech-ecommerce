debfdfb1423f1f776665a333ba26f94422885396
keyvan-dev <keyvanfordev@gmail.com>
2024-12-09 08:48:56 +0330

    Add a toggle color mode component

---
+++ client/src/components/ColorModeToggle.jsx
@@ -0,0 +1,13 @@
+// Chakra UI
+import { useColorMode, IconButton } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+
+const ColorModeToggle = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	return (
+		<IconButton icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />} onClick={toggleColorMode} variant='ghost' />
+	);
+};
+
+export default ColorModeToggle;
